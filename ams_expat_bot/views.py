5591527e5e732fed621acb9b3a3ef97a5aa99c1d
Amina Keldibek <amina.keldibek@gmail.com>
2019-10-20 22:59:23 +0200

    add mvp

--- ams_expat_bot/views.py
+++ ams_expat_bot/views.py
@@ -10,13 +10,26 @@ from django.conf import settings
 @require_POST
 @csrf_exempt
 def webhook(request):
+    # responces
+    responce = "How can I help you?"
+
+    bureaucracy_resp = """/residence_permit\n/BSN\n/DigiD\n/registration\n/driverLicence"""
+    residence_permit_resp = "https://www.iamexpat.nl/expat-info/official-issues/residence-permit-netherlands"
     telegram_url = "https://api.telegram.org"
+
     data = json.loads(request.body)
     print (data)
+
+    text = data['message']['text']
+    if text == "/Bureaucracy":
+        responce = bureaucracy_resp
+    if text == "/residence_permit":
+        responce = residence_permit_resp
+
     r = requests.post(
         telegram_url + "/bot" + settings.TELEGRAM_TOKEN + "/sendMessage",
         data={"chat_id": data['message']['chat']['id'],
-              "text": "Hi back"}
+              "text": responce}
     )
     print (r)
 
